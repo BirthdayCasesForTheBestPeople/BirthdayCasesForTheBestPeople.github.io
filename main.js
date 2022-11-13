d74e1dda835c400ef6856922507190f095f43c99
Yurii <yurii.skarbek@gmail.com>
2022-11-13 19:21:02 +0200

    Init

---
+++ main.js
@@ -0,0 +1,66 @@
+const presentList = [
+    {
+        name: "Блендер",
+        img: "./img/blenders.jpg"
+    },
+    {
+        name: "Хлібопічка",
+        img: "./img/breadoven.jpg"
+    },
+    {
+        name: "Кавоварка",
+        img: "./img/coffee.jpg"
+    },
+    {
+        name: "Гриль",
+        img: "./img/grill.jpg"
+    },
+    {
+        name: "Соковижималка",
+        img: "./img/juice.jpg"
+    },
+    {
+        name: "Чайник",
+        img: "./img/kittels.jpg"
+    },
+    {
+        name: "Мікрохвильовка",
+        img: "./img/microwave.jpg"
+    },
+    {
+        name: "Міксeh",
+        img: "./img/mixers.jpg"
+    },
+    {
+        name: "Мультиварка",
+        img: "./img/multi.jpg"
+    },
+    {
+        name: "Тосер",
+        img: "./img/tosters.jpg"
+    }
+]
+
+console.log("sdfsd")
+
+const list = document.getElementById("roller1");
+for(let i = 0; i < presentList.length; i++){
+    list.innerHTML += `<div class="roller-element" style="background-image: url(${presentList[i].img})"></div>`;
+}
+for(let i = 0; i < presentList.length; i++){
+    list.innerHTML += `<div class="roller-element" style="background-image: url(${presentList[i].img})"></div>`;
+}
+for(let i = 0; i < presentList.length; i++){
+    list.innerHTML += `<div class="roller-element" style="background-image: url(${presentList[i].img})"></div>`;
+}
+for(let i = 0; i < presentList.length; i++){
+    list.innerHTML += `<div class="roller-element" style="background-image: url(${presentList[i].img})"></div>`;
+}
+for(let i = 0; i < presentList.length; i++){
+    list.innerHTML += `<div class="roller-element" style="background-image: url(${presentList[i].img})"></div>`;
+}
+
+const test = () => {
+    console.log("test");
+    list.classList.add("roll-animation1");
+}
